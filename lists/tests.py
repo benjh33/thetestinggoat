dee200b00216afca8fd815cea6d6b89f90727b44
Ben Hunter <bjameshunter@gmail.com>
2015-04-15 18:22:39 -0500

    handled simple POST request

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -14,4 +16,17 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',
+                    { "new_item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
         self.assertEqual(response.content.decode(), expected_html)
