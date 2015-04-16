4ba43831e597cfa636184212995653efeb730a14
Ben Hunter <bjameshunter@gmail.com>
2015-04-16 17:15:40 -0500

    refactor templates to use base

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -87,6 +87,8 @@ class NewVisitorTestCase(LiveServerTestCase):
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing...')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']//2,
             512, delta = 5)
