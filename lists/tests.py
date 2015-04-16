029119d70a20383ca8c6ab706e0df404a041a706
Ben Hunter <bjameshunter@gmail.com>
2015-04-15 19:37:31 -0500

    Redirect after post, show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -47,8 +47,32 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string('home.html',
-                    { "new_item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_home_page_only_saves_list_when_needed(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        item1 = Item.objects.create(text = "itemy 1")
+        item2 = Item.objects.create(text = "itemy 2")
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemy 1', response.content.decode())
+        self.assertIn('itemy 2', response.content.decode())
