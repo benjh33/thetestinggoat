4d25c445c73a861aaf8bea81857f6138c730f7bc
Ben Hunter <bjameshunter@gmail.com>
2015-04-16 10:32:11 -0500

    added new user and browser session to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -43,8 +43,38 @@ class NewVisitorTestCase(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # new user visits
+        ## get rid of old browser and start a new one
+        ## no cookies from edith's session
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        # Francis starts a new list 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(keys.ENTER)
+
+        # Francis gets a unique url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # still no trace of Edith's text
+        page_text = self.browser.get_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # There is still a text box inviting her to add another item.
         # enters "Use peacock feathers to make a fly"
