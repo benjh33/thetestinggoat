6c94dc19454f78b09ec171bf94bce9691d21ae20
Ben Hunter <bjameshunter@gmail.com>
2015-04-16 18:17:05 -0500

    setup functional tests to work with staging server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,26 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 import unittest
 import time
 
 class NewVisitorTestCase(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = "http://" + arg.split("=")[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+        self.browser.quit()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +37,7 @@ class NewVisitorTestCase(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -54,7 +70,7 @@ class NewVisitorTestCase(StaticLiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
@@ -83,7 +99,7 @@ class NewVisitorTestCase(StaticLiveServerTestCase):
         # Satisfied, she goes back to sleep
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
